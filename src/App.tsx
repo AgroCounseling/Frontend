970db6e13f0f20583206ab2b604932839f073117
Aman <aman_A00@mail.ru>
2020-06-08 23:44:56 +0600

    add main packages redux react-redux sass styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import './App.css'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 const App = () => {
-  return (
-    <div className="App">
-        <Router>
-            <Switch>
-                <Route exact path={'/'}>
-                    Hello World
-                </Route>
-
-            </Switch>
-        </Router>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path={'/'}>
+                        Agrariev
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
