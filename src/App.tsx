fb85ff77c18ad2e74d68100db8ac606e2901fe68
Aman <aman_A00@mail.ru>
2020-06-24 20:57:49 +0600

    add styled-components and device size

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom"
 import {initialise} from "./state/appReducer";
 import {connect} from "react-redux";
 import {GlobalStateType} from "./state/root-reducer";
-import {getToken, isPending} from "./state/selectors";
+import {isPending} from "./state/selectors";
+import Header from "./components/header/Header";
+import AuthPage from "./components/authentication/AuthPage";
+import {RegisterFormConsultant} from "./components/authentication/sign-up/SignUpForms";
+import {SignIn} from "./components/authentication/sign-in/SignInForm";
+import Preloader from "./components/preloader/Preloader";
+import NavBar from "./components/navbar/NavBar";
+import MainPage from "./components/mainPage/MainPage";
 
-const App = (props:any) => {
+
+type OwnProps = {
+    isPending: boolean
+    initialise: () => void
+}
+
+const App = (props:OwnProps) => {
+    const [pending, setPending] = useState(true)
     const allPromiseRejection = (promiseRejectionEvent: any) =>{
         alert(promiseRejectionEvent)
     }
@@ -18,12 +32,37 @@ const App = (props:any) => {
         }
     }, [props])
 
+    setTimeout(()=>{
+        setPending(false)
+    },1500)
+    if(pending){
+        return <div className={'preloaderWrapper'}><Preloader /></div>
+    }
     return (
         <div className="App">
             <Router>
+                <Header/>
                 <Switch>
                     <Route exact path={'/'}>
+                        <NavBar/>
+                        <MainPage/>
                     </Route>
+                    <Route exact path={'/sign-in'}>
+                        <AuthPage>
+                            <SignIn/>
+                        </AuthPage>
+                    </Route>
+                    <Route path={'/sign-up-consultant'}>
+                        <AuthPage>
+                            <RegisterFormConsultant />
+                        </AuthPage>
+                    </Route>
+                    <Route path={'/admin'}>
+                        <div style={{marginTop: '63px'}}>
+                            Admin
+                        </div>
+                    </Route>
+                    <Redirect to={'/'}/>
                 </Switch>
             </Router>
         </div>
@@ -32,7 +71,6 @@ const App = (props:any) => {
 
 export default connect((state: GlobalStateType) => {
     return {
-        token: getToken(state),
         isPending: isPending(state)
     }
 },{initialise})(App)
