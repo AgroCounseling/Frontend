17e8ad2ed08ec183b8f1e1b7b16c76cd1c3f628f
Sardar Atavekov <sardarbekatabekov@gmail.com>
2020-08-16 18:49:43 +0600

    begin chat page

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -4,16 +4,26 @@ import send from '../../img/Mask.png';
 import plus from '../../img/plus.png';
 import noPic from "../../img/noPicture.png";
 import Api from "./../../api/Api";
+import { Time } from "./../functions/time.js";
 type ChatType = {
     id?: number
 }
 const Chat: React.FC<ChatType> = ({ id }) => {
     let [rooms, setRooms] = useState([])
     useEffect(() => {
-        Api.getRooms().then(res => setRooms(res.data));
+        Api.getRooms().then((res: any) => {
+            let data = res.data.map((item: any) => {
+                console.log('item', item);
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                item.user = item.first.id != id ? "first" : "second";
+                return item;
+            })
+            setRooms(data);
+        }
+        );
     }, [])
 
-
+    console.log('rooms', rooms, id);
     return (
         <div className={css.chatWrapper}>
             <div>
@@ -22,12 +32,12 @@ const Chat: React.FC<ChatType> = ({ id }) => {
                 </div>
                 <div className={css.userList}>
                     {
-                        rooms.map((item:any) => <User key={item.id} time={item.timestamp} image={item.second.photo} name={item.second.first_name + ' ' + item.second.last_name} />)
+                        rooms.map((item: any) => <User key={item.id} time={item.timestamp} image={item[item.user].photo} name={item[item.user].first_name + ' ' + item[item.user].last_name} />)
                     }
                 </div>
             </div>
             <div>
-                <MessageBlock />
+
             </div>
         </div>
     )
@@ -40,15 +50,15 @@ type UserProps = {
     image: string
     time: string
 }
-const User = (props:UserProps) => {
+const User = (props: UserProps) => {
     return (
         <div className={css.personWrapper}>
             <div className={css.person}>
-                <div>
-                    <img src={props.image ? props.image : noPic} alt="#" />
+                <div className={css.person}>
+                    <img src={props.image ? props.image : noPic} alt="#" className={css.personImg} />
+                    <div className={css.personName}>{props.name}</div>
                 </div>
-                <div className={css.personName}>{props.name}</div>
-                <div className={css.peronTime}>{props.time}</div>
+                <div className={css.peronTime}>{Time(props.time)}</div>
             </div>
             <div className={css.textWrapper}>
                 <div>
