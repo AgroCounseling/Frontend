cad846f5fe74e9c051b4a2d8f30e5e95245d88db
Aman <aman_A00@mail.ru>
2020-08-19 18:32:12 +0600

    chat: getting, sending message and  small refactorings

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,49 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import css from "./chat.module.css";
 import send from '../../img/Mask.png';
 import plus from '../../img/plus.png';
 import noPic from "../../img/noPicture.png";
 import Api from "./../../api/Api";
-import { Time } from "../functions/time";
+import {Time} from "../functions/time";
+import {useDispatch} from "react-redux";
+import {checkToken} from "../../state/authReducer";
 
 type ChatType = {
     id: number
 }
-const Chat: React.FC<ChatType> = ({ id }) => {
+const Chat: React.FC<ChatType> = ({id}) => {
     let [rooms, setRooms] = useState<any>([])
     const [current, setCurrent] = useState(0)
     const [email, setEmail] = useState('');
     useEffect(() => {
         Api.getRooms().then((res: any) => {
-            let data = res.data.map((item: any) => {
-                item.user = item.first.id != id ? "first" : "second";
-                return item;
-            })
-            setRooms(data);
-        }
+                let data = res.data.map((item: any) => {
+                    item.user = item.first.id != id ? "first" : "second";
+                    return item;
+                })
+                setRooms(data);
+            }
         );
     }, [])
-
-    const setCur = (e:any) => {
-        setCurrent(e)
-    }
 
     return (
         <div className={css.chatWrapper}>
             <div>
                 <div className={css.searchWrapper}>
-                    <input type="text" placeholder={'искать'} />
+                    <input type="text" placeholder={'искать'}/>
                 </div>
                 <div className={css.userList}>
                     {
                         rooms.map((item: any) => <User
+                            current={current}
                             key={item.id}
                             id={item.id}
                             setCurrent={setCurrent}
                             setEmail={setEmail}
                             email={item[item.user].email}
                             time={item.timestamp} image={item[item.user].photo}
-                            name={item[item.user].first_name + ' ' + item[item.user].last_name} />)
+                            name={item[item.user].first_name + ' ' + item[item.user].last_name}/>)
                     }
                 </div>
             </div>
@@ -68,16 +67,18 @@ type UserProps = {
     id: number
     email: string
     setEmail: any;
+    current: number
 }
 const User = (props: UserProps) => {
     return (
-        <div className={css.personWrapper} onClick={() => {
-            props.setCurrent(props.id);
-            props.setEmail(props.email);
-        }}>
+        <div className={props.current === props.id ? css.activeUser + ' ' + css.personWrapper : css.personWrapper}
+             onClick={() => {
+                 props.setCurrent(props.id);
+                 props.setEmail(props.email);
+             }}>
             <div className={css.person}>
                 <div className={css.person}>
-                    <img src={props.image ? props.image : noPic} alt="#" className={css.personImg} />
+                    <img src={props.image ? props.image : noPic} alt="#" className={css.personImg}/>
                     <div className={css.personName}>{props.name}</div>
                 </div>
                 <div className={css.peronTime}>{Time(props.time)}</div>
@@ -101,43 +102,66 @@ type MessageProps = {
     userId: number
     email: string
 }
-const MessageBlock = (props: MessageProps) => {
+const MessageBlock: React.FC<MessageProps> = ({id, ...props}) => {
+    const dispatch = useDispatch()
+
     const [inp, setInp] = useState('')
     const [data, setData] = useState<any>(null)
     const [user, setUser] = useState<any>(null)
-    const [messages, setMessages] = useState<any>(null)
+    const [messages, setMessages] = useState<any>([])
+    const messageId: any = useRef(null)
+
+    const Send = async (req: any) => {
+        return dispatch(checkToken(() => req))
+    }
+    const scrollToBottom = () => {
+        const scrollHeight = messageId.current.scrollHeight;
+        const height = messageId.current.clientHeight;
+        messageId.current.scrollTop = scrollHeight - height;
+    }
 
     const submit = (e: any) => {
         e.preventDefault()
-        Api.sendMessage(props.email, { message: inp })
-            .then((res) => {
-                console.log(res.data)
+        Send(Api.sendMessage(props.email, {message: inp}))
+            .then((res: any) => {
                 let a = {
                     ...res.data,
-                    id: props.userId,
+                    user: props.userId,
                 }
                 setMessages([...messages, a])
+                setInp('')
+                scrollToBottom()
+            })
+    }
+
+
+    const getRoom = (scroll?: boolean) => {
+        Send(Api.getRooms(id))
+            .then((res: any) => {
+                setData(res.data)
+                setMessages(res.data.messages)
+                if (+res.data.first.id === +props.userId) {
+                    setUser(res.data.second)
+                } else {
+                    setUser(res.data.first)
+                }
+                scrollToBottom()
             })
     }
     useEffect(() => {
-        Api.getRooms(props.id)
-            .then((res) => {
-                console.log('res', res);
-                setData(res.data)
-                setMessages(res.data.messages)
-                if(+res.data.first.id === +props.userId){
-                    setUser(res.data.second)
-                }else{
-                    setUser(res.data.first)
-                }
-            })
-    }, [props.id])
+        getRoom(true)
+    }, [id])
+
+    useEffect(() => {
+        let a = setInterval(() => getRoom(), 5000)
+        return () => clearInterval(a)
+    }, [id])
     return (
         <div className={css.message__wrapper}>
             <div className={css.chatHeader}>
                 <div>
                     <div className={css.avaWrapper}>
-                        <img src={user?.photo ? user.photo : noPic} alt="#" />
+                        <img src={user?.photo ? user.photo : noPic} alt="#"/>
                     </div>
                     <div className={css.personName}>{user?.first_name + ' ' + user?.last_name}</div>
                 </div>
@@ -146,21 +170,24 @@ const MessageBlock = (props: MessageProps) => {
                     <button>B</button>
                 </div>
             </div>
-            <div className={css.messages}>
+            <div ref={messageId} className={css.messages}>
                 {
                     messages
-                        ? messages?.map((item:any) => <div key={item.id}>{item.message}</div> )
+                        ? messages?.map((item: any) => <div key={item.id}
+                                                            className={item.user === props.userId ? css.myMessageWrapper : css.messageWrapper}>
+                            <div className={item.user === props.userId ? css.myMessage : css.message}>{item.message}</div>
+                        </div>)
                         : null
                 }
             </div>
             <form onSubmit={submit} className={css.message__input}>
                 <input placeholder={'Введите ваше сообщение'} value={inp} onChange={(e) => setInp(e.target.value)}
-                    type="text" />
+                       type="text"/>
                 <span className={css.plus}>
-                    <img src={plus} alt="+" />
+                    <img src={plus} alt="+"/>
                 </span>
                 <span onClick={submit} className={css.send}>
-                    <img src={send} alt="send" />
+                    <img src={send} alt="send"/>
                 </span>
             </form>
         </div>
